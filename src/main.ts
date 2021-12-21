b525bfe9a758279e3678be11ba4960a0f4c5ab8d
himanshu-shipmnts <himanshu@shipmnts.com>
2021-12-21 12:46:04 +0530

    cors enebale

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as express from 'express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors: true });
   app.use(express.static(join(process.cwd(), './public')))
   await app.listen(process.env.PORT || 3000);
 }
